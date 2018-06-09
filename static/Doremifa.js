9fdedc57839c3f1a49b1f9fcef06564a553c641b
Tero Tolonen <tero@sumpli.com>
2018-06-09 16:15:23 +0300

    array can not be a root node

--- static/Doremifa.js
+++ static/Doremifa.js
@@ -195,7 +195,6 @@ var drmfTemplate = /** @class */ (function () {
                     // transform into txt node
                     if (typeof (value) == 'string') {
                         var txt = document.createTextNode(value);
-                        this_1.slotTypes[i] = [3, last_root, txt];
                         var nodes_2 = currTpl.rootNodes;
                         var pNode = nodes_2[0].parentNode;
                         var first = nodes_2[0];
@@ -204,6 +203,7 @@ var drmfTemplate = /** @class */ (function () {
                             var n = nodes_3[_i];
                             pNode.removeChild(n);
                         }
+                        this_1.slotTypes[i] = [3, pNode, txt];
                     }
                     break;
                 case 3:
@@ -283,7 +283,6 @@ var drmfTemplate = /** @class */ (function () {
                     if (typeof (value) == 'string') {
                         var tplNow = last_slot[3];
                         var txt = document.createTextNode(value);
-                        this_1.slotTypes[i] = [3, last_root, txt];
                         var nodes_4 = tplNow.rootNodes;
                         var pNode = nodes_4[0].parentNode;
                         var first = nodes_4[0];
@@ -292,6 +291,7 @@ var drmfTemplate = /** @class */ (function () {
                             var n = nodes_5[_c];
                             pNode.removeChild(n);
                         }
+                        this_1.slotTypes[i] = [3, pNode, txt];
                     }
                     if (value instanceof drmfTemplate) {
                         var comp = last_slot[2];
@@ -433,6 +433,14 @@ var drmfTemplate = /** @class */ (function () {
                 if (value instanceof drfmKey) {
                     return;
                 }
+                var append = function (new_node) {
+                    if (activeNode) {
+                        activeNode.appendChild(new_node);
+                    }
+                    else {
+                        me.rootNodes.push(new_node);
+                    }
+                };
                 if (index & 1) {
                     if (value instanceof drmfTemplate) {
                         var tpl = value;
@@ -440,7 +448,7 @@ var drmfTemplate = /** @class */ (function () {
                         var snodes = [];
                         for (var _i = 0, items_1 = items; _i < items_1.length; _i++) {
                             var it = items_1[_i];
-                            activeNode.appendChild(it);
+                            append(it);
                             snodes.push(it);
                         }
                         // render template
@@ -454,7 +462,7 @@ var drmfTemplate = /** @class */ (function () {
                         var snodes = [];
                         for (var _a = 0, items_2 = items; _a < items_2.length; _a++) {
                             var it = items_2[_a];
-                            activeNode.appendChild(it);
+                            append(it);
                             snodes.push(it);
                         }
                         // render template
@@ -465,7 +473,7 @@ var drmfTemplate = /** @class */ (function () {
                         var coll = new drmfTemplateCollection;
                         var txtV = document.createTextNode('');
                         coll.node = txtV;
-                        activeNode.appendChild(txtV); // placeholder in case empty list
+                        append(txtV); // placeholder in case empty list
                         var tpls = value;
                         coll.list = tpls;
                         var snodes = [];
@@ -474,10 +482,13 @@ var drmfTemplate = /** @class */ (function () {
                             if (!cont || !cont.createDOM) {
                                 throw "Array or result of map must contain valid template elements:\n " + value + " \n----------------------------\n " + me.valuestream;
                             }
+                            if (!activeNode) {
+                                throw "Array can not be root node of html:\n " + value + " \n----------------------------\n " + me.valuestream;
+                            }
                             var items = cont.createDOM();
                             for (var _b = 0, items_3 = items; _b < items_3.length; _b++) {
                                 var it = items_3[_b];
-                                activeNode.appendChild(it);
+                                append(it);
                                 snodes.push(it);
                             }
                         }
@@ -538,7 +549,7 @@ function html(strings) {
         values[_i - 1] = arguments[_i];
     }
     var t = new drmfTemplate();
-    t.key = strings.join('<>');
+    t.key = strings.join('&');
     t.strings = strings;
     t.values = values.map(function (value) {
         if (typeof (value) === 'undefined')
@@ -637,7 +648,6 @@ function mount(root, comp,
 state, options) {
     var _this = this;
     if (!app.is_registered) {
-        console.log('registering app');
         app.is_registered = true;
         register_hash();
         window.addEventListener("hashchange", register_hash, false);
@@ -789,7 +799,7 @@ var XMLParser = /** @class */ (function () {
         this.buff_index = 0;
         this.used_index = 0;
         this.eof = false;
-        if (!this.buff)
+        if (typeof (this.buff) === 'undefined')
             this.eof = true;
     }
     XMLParser.prototype.code = function (index) {
@@ -996,6 +1006,12 @@ var XMLParser = /** @class */ (function () {
         var cc1 = 0;
         var cc2 = 0;
         while (!this.eof) {
+            if (typeof (this.buff) === 'string' && this.buff.length === 0) {
+                var idx = this.buff_index;
+                callback.addTextNode('', idx);
+                this.stepBuffer();
+                continue;
+            }
             cc1 = this.here();
             if (this.in_tagdef) {
                 // <div  something = "..."
